977772a2983fc8aeba6f64d3ff04793b4ce5d01a
Will Johnson <wjohnson@whamcloud.com>
2019-09-24 22:01:23 -0400

    - Use `expect` to answer script prompts and complete the installation
    Signed-off-by: Will Johnson <wjohnson@whamcloud.com>

--- scripts/install_iml_tar.sh
+++ scripts/install_iml_tar.sh
@@ -1,4 +1,31 @@
 mkdir -p /tmp/iml-install
 cd /tmp/iml-install
 curl -L $1 | tar zx --strip 1
-yes [admin, lustre, lustre,,] | ./install --no-dbspace-check
+yum install -y expect
+/usr/bin/expect << EOF
+set timeout -1
+
+spawn ./install --no-dbspace-check
+
+expect "Username: "
+
+send -- "admin\r"
+
+expect "Password: "
+
+send -- "lustre\r"
+
+expect "Confirm password: "
+
+send -- "lustre\r"
+
+expect "Email: "
+
+send -- "\r"
+
+expect "NTP Server *: "
+
+send -- "\r"
+
+expect eof
+EOF

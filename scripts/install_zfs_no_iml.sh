b57e1e42c7572199c6d2949d5ddbe2b16469af46
Joe Grund <jgrund@whamcloud.io>
2019-11-08 17:52:59 -0500

    Ensure zed is started when creating pools
    Signed-off-by: Joe Grund <jgrund@whamcloud.io>

--- scripts/install_zfs_no_iml.sh
+++ scripts/install_zfs_no_iml.sh
@@ -4,3 +4,4 @@ yum-config-manager --add-repo=https://downloads.whamcloud.com/public/lustre/lust
 yum-config-manager --add-repo=https://downloads.whamcloud.com/public/e2fsprogs/latest/el7/
 yum-config-manager --add-repo=http://download.zfsonlinux.org/epel/7.6/kmod/x86_64/
 yum install -y --nogpgcheck lustre zfs kmod-lustre-osd-ldiskfs kmod-lustre-osd-zfs
+systemctl enable --now zfs-zed
